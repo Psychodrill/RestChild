62453ebfe4b1db80e7c2051d50bc328637e74bfb
Govorov Pavel <govorov@adapt.ru>
2021-10-01 18:26:44 +0300

    Задача "Реализовать функционал по учету использования сертификатов".

---
+++ RestChild.Web/Controllers/CertificateSearchController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Mvc;
+using RestChild.Booking.Logic.Extensions;
+using RestChild.Booking.Logic.Services;
+using RestChild.Comon;
+using RestChild.Comon.Config;
+using RestChild.Comon.Dto;
+using RestChild.Comon.Dto.Booking;
+using RestChild.Comon.Dto.SearchRestChild;
+using RestChild.Comon.Enumeration;
+using RestChild.DAL;
+using RestChild.DAL.RepositoryExtensions;
+using RestChild.Domain;
+using RestChild.Extensions.Extensions;
+using RestChild.Extensions.Filter;
+using RestChild.Security.Logger;
+using RestChild.Web.Controllers.WebApi;
+using RestChild.Web.Extensions;
+using RestChild.Web.Models;
+using RestChild.Web.Models.Business.Export;
+using RestChild.Web.Properties;
+using TimeOfRest = RestChild.Domain.TimeOfRest;
+
+namespace RestChild.Web.Controllers
+{
+  
+
+    /// <summary>
+    ///     управление заявочной кампанией
+    /// </summary>
+    [System.Web.Mvc.Authorize]
+    public partial class CertificateSearchController : BaseController
+    {
+        private readonly string DefaultOptionValue = "-- Не выбрано --";
+        public WebCertificateSearchController ApiController { get; set; }
+        public WebVocabularyController VocController { get; set; }
+        public WebBtiDistrictsController DistrictController { get; set; }
+        public WebHotelsController HotelController { get; set; }
+        public WebContractController ContractController { get; set; }
+
+
+        public ActionResult CertificateList(CertificateFilterModel model)
+        {
+            if (!Security.HasAnyRightsForSomeOrganization(new[] { AccessRightEnum.RequestView }))
+            {
+                return RedirectToAvalibleAction();
+            }
+
+            SetUnitOfWorkInRefClass(UnitOfWork);
+
+            PrepareVocabulary(null, model);
+            model = model ?? new CertificateFilterModel();
+
+            if (model.PageSize == 0 || model.PageNumber == 0)
+            {
+                model.PageSize = 10;
+                model.PageNumber = 1;
+            }
+
+            if (!model.HotelsId.IsNullOrEmpty())
+            {
+                model.Hotels = UnitOfWork.GetById<Hotels>(model.HotelsId.Value);
+            }
+
+            return View(ApiController.CertificateList(model));
+        }
+
+        /// <summary>
+        ///     заполнение справочников во ViewBag
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="filter"></param>
+        private void PrepareVocabulary(Certificate certificate = null, CertificateSearchFilterModel filter = null)
+        {
+            ViewBag.Organizations =
+                HotelController.Get().OrderBy(p => p.Id)
+                .InsertAt(new Hotels { Id = 0, Name = DefaultOptionValue }, 0);
+
+            ViewBag.PlacesOfRest =
+                VocController.GetPlacesOfRestInternal(true).OrderBy(p => p.Id)
+                    .InsertAt(new PlaceOfRest { Id = 0, Name = DefaultOptionValue });
+
+            ViewBag.Contracts =
+                UnitOfWork.GetAll<Contract>().Where(c=>c.OrganizationId.HasValue).OrderBy(p => p.Id)
+                .InsertAt(new Contract { Id = 0, SignNumber = DefaultOptionValue },0);
+
+            ViewBag.StatusOfRest =
+                VocController.GetStatusOfRest()
+                    .Where(s => s.Id != (long)StatusEnum.ErrorRequest && s.ForPreferential && s.IsFinal)
+                    .OrderBy(p => p.Id == (long)StatusEnum.Draft ? 0 : 1)
+                    .ThenBy(p => p.Name)
+                    .ToList()
+                    .InsertAt(new Status {Id = 0, Name = DefaultOptionValue },0);
+        }
+
+        /// <summary>
+        /// Поиск сертификата
+        /// </summary>
+        public ActionResult CertificateView(long id)
+            {
+                if (!Security.HasAnyRightsForSomeOrganization(new[] { AccessRightEnum.RequestView }))
+                {
+                    return RedirectToAvalibleAction();
+                }
+                var certificate = UnitOfWork.GetById<Certificate>(id);
+                return View(certificate);
+            }
+
+        private string OrderString(TypeOfRest t)
+        {
+            var res = string.Empty;
+            while (t != null)
+            {
+                res = t.Name + "\\" + res;
+                t = t.Parent;
+            }
+
+            return res;
+        }
+
+        private TypeOfRest GetLeveled(HashSet<long> ids, TypeOfRest t)
+        {
+            var item = t;
+            var level = 0;
+            while (item != null)
+            {
+                level++;
+                item = item.Parent;
+            }
+
+            return t != null ? new TypeOfRest(t) { MaxAge = level, IsActive = ids.Contains(t.Id) } : null;
+        }
+
+    }
+}
---
+++ RestChild.Web/Controllers/WebApi/WebCertificateSearchController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using RestChild.Comon;
+using RestChild.Comon.Enumeration;
+using RestChild.DAL.RepositoryExtensions;
+using RestChild.Domain;
+using RestChild.Extensions.Filter;
+
+namespace RestChild.Web.Controllers.WebApi
+{
+    /// <summary>
+    ///     Контроллер для работы с заявлением.
+    /// </summary>
+    [Authorize]
+    public class WebCertificateSearchController : BaseController
+    {
+        public WebCalculationController ApiCalculationController { get; set; }
+
+        public override void SetUnitOfWorkInRefClass(IUnitOfWork unitOfWork)
+        {
+            base.SetUnitOfWorkInRefClass(unitOfWork);
+            ApiCalculationController.SetUnitOfWorkInRefClass(unitOfWork);
+        }
+
+        [ActionName("CertificateList")]
+        public CertificateFilterModel CertificateList(CertificateFilterModel search, bool needPaging = true)
+        {
+            SetUnitOfWorkInRefClass(UnitOfWork);
+
+            var pager = new PagerState(search.PageNumber, search.PageSize);
+            var query = CertificateListQuery(search);
+
+            query = query.Include(r => r.Request.Applicant)
+                .Include(r => r.Request.TimeOfRest)
+                .Include(r => r.Request.TypeOfRest)
+                .Include(r => r.Request.Status)
+                .Include(r => r.Request.Tour)
+                .Include(r => r.Request.PlaceOfRest)
+                .Include(r => r.Request.Tour.Hotels)
+                .Include(r => r.Request.TypeOfRest.Parent)
+                .Include(r => r.Request.Attendant);
+            if (needPaging)
+            {
+                search.Certificates = new CommonPagedList<Certificate>(
+                    query.GetPage(pager),
+                    pager.CurrentPage,
+                    pager.PerPage,
+                    query.Count());
+            }
+            else
+            {
+                search.Certificates = new CommonPagedList<Certificate>(
+                    query.ToList(),
+                    1,
+                    1,
+                    query.Count());
+            }
+
+            search.TotalRecordsCount =
+                query.Select(x => x.Request.Child.Count(c => !c.IsDeleted) + x.Request.Attendant.Count + (x.Request.Applicant.IsAccomp ? 1 : 0))
+                    .DefaultIfEmpty().Sum();
+
+            return search;
+        }
+        internal IQueryable<Certificate> CertificateListQuery(CertificateFilterModel search)
+        {
+            if (search.WillRest == null)
+                search.WillRest = false;
+            if (search.Resting == null)
+                search.Resting = false;
+            if (search.Rested == null)
+                search.Rested = false;
+            if (search.AttendantWith == null)
+                search.AttendantWith = false;
+            if (search.AttendantWO == null)
+                search.AttendantWO = false;
+            var query =
+                UnitOfWork.GetSet<Certificate>().Where(r =>r.Request.StatusId==(long)StatusEnum.CertificateIssued);
+
+            if (!string.IsNullOrWhiteSpace(search.ApplicantFio))
+            {
+                var item = search.ApplicantFio.ToLower().Trim().Replace(' ', '|').Replace("ё", "е");
+
+                query = query.Where(r => r.Request.Applicant.Key.Contains(item));
+            }
+
+            if (!string.IsNullOrWhiteSpace(search.ChildFio))
+            {
+                var item = search.ChildFio.ToLower().Trim().Replace(' ', '|').Replace("ё", "е");
+
+                query = query.Where(r => r.Request.Child.Any(c => c.Key.Contains(item)));
+            }
+
+            if (!string.IsNullOrWhiteSpace(search.AttendantFio))
+            {
+                var item = search.AttendantFio.ToLower().Trim().Replace(' ', '|').Replace("ё", "е");
+
+                query = query.Where(r => r.Request.Attendant.Any(c => c.Key.Contains(item)));
+            }
+
+            if (!string.IsNullOrWhiteSpace(search.ContractNumber))
+            {
+                query = query.Where(r => r.ContractNumber == search.ContractNumber);
+            }
+
+            if (!search.Hotels.IsNullOrEmpty())
+            {
+                query = query.Where(r => r.Place == search.Hotels.NameOrganization);
+            }
+
+            if (search.PlaceOfRestId > 0)
+            {
+                query = query.Where(q => q.Request.PlaceOfRestId == search.PlaceOfRestId);
+            }
+
+            if (search.ContractDate.HasValue)
+            {
+                query = query.Where(q => q.ContractDate == DbFunctions.TruncateTime(search.ContractDate.Value));
+            }
+            if (!search.Request.IsNullOrEmpty())
+                if (search.Request.CertificateDate.HasValue)
+            {
+                query = query.Where(q => DbFunctions.TruncateTime(q.Request.CertificateDate) == DbFunctions.TruncateTime(search.Request.CertificateDate.Value));
+            }
+            if (search.RestDateFromFrom.HasValue)
+            {
+                query = query.Where(q => q.RestDateFrom >= DbFunctions.TruncateTime(search.RestDateFromFrom.Value));
+            }
+            if (search.RestDateFromTo.HasValue)
+            {
+                query = query.Where(q => q.RestDateFrom <= DbFunctions.TruncateTime(search.RestDateFromTo.Value));
+            }
+            if (search.RestDateToFrom.HasValue)
+            {
+                query = query.Where(q => q.RestDateFrom >= DbFunctions.TruncateTime(search.RestDateFromFrom.Value));
+            }
+            if (search.RestDateToTo.HasValue)
+            {
+                query = query.Where(q => q.RestDateFrom <= DbFunctions.TruncateTime(search.RestDateFromTo.Value));
+            }
+            if ((bool)search.WillRest)
+            {
+                query = query.Where(q => q.RestDateFrom >= DateTime.Now);
+            }
+            if ((bool)search.Resting)
+            {
+                query = query.Where(q => q.RestDateFrom <= DateTime.Now && q.RestDateTo >= DateTime.Now);
+            }
+            if ((bool)search.Rested)
+            {
+                query = query.Where(q => q.RestDateTo <= DateTime.Now);
+            }
+            if ((bool)search.AttendantWith)
+            {
+                query = query.Where(q => q.Request.Attendant.Any());
+            }
+            if (search.ContractId>0)
+            {
+                var contract = UnitOfWork.GetById<Contract>(search.ContractId);
+                query = query.Where(q => q.Request.OrganizationId == contract.OrganizationId);
+            }
+            if (!string.IsNullOrWhiteSpace(search.SNILS))
+            {
+                query = query.Where(r =>
+                    r.Request.Applicant.Snils.Contains(search.SNILS)
+                    || r.Request.Child.Any(c => c.Snils.Contains(search.SNILS))
+                    || r.Request.Attendant.Any(c => c.Snils.Contains(search.SNILS))
+                    || r.Request.Agent.Snils.Contains(search.SNILS)
+                );
+            }
+            return query;
+        }
+
+    }
+}
