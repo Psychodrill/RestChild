ecb895636a778320d0a945171acb8ab28047e3fe
Govorov Pavel <govorov@adapt.ru>
2021-10-13 14:02:44 +0300

    Фикс для логики поиска дублей сопровождающих.

--- RestChild.Web/Controllers/WebApi/WebFirstRequestCompanyController.cs
+++ RestChild.Web/Controllers/WebApi/WebFirstRequestCompanyController.cs
@@ -1161,7 +1161,7 @@ namespace RestChild.Web.Controllers.WebApi
             //{
             model.SameAttendants = attendants
                 .Where(a => !model.SameAttendantSnils.Contains(a.Snils ?? string.Empty)
-                        || applicants.Any(s => s.Applicant.Snils == a.Snils || s.Attendant.Any(at => at.Snils == a.Snils)))
+                        && applicants.Any(s => s.Applicant.Snils == a.Snils || s.Attendant.Any(at => at.Snils == a.Snils)))
                 //.GroupBy(g => (g.DocumentSeria ?? string.Empty) + "---" + (g.DocumentNumber ?? string.Empty))
                 //.Where(g => g.Count() > 1).SelectMany(g => g.Select(f => f).ToList())
                 .ToList();
