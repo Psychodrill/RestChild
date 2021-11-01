53ea6c90fabd2dcf609ed660043a9490c6b47fdc
Govorov Pavel <govorov@adapt.ru>
2021-11-01 16:31:52 +0300

    Фикс для костыля с доверенным лицом.

--- RestChild.Web/Controllers/WebApi/WebExchangeController.cs
+++ RestChild.Web/Controllers/WebApi/WebExchangeController.cs
@@ -1841,6 +1841,8 @@ namespace RestChild.Web.Controllers.WebApi
                         {
                             vm.SameAttendants.Clear();
                             vm.SameAttendantSnils.Clear();
+                            if (vm.Data.TypeOfRestId != (long)TypeOfRestEnum.YouthRestCamps && vm.Data.TypeOfRestId != (long)TypeOfRestEnum.YouthRestCamps)
+                            vm.ApplicantDouble.Clear();
                         }
                             if (vm.SameAttendantSnils.Any() || vm.SameAttendants.Any())
                         {
